d861564c6e2f080b98f9e73e3f97ccff39851d9c
natnew <natashanewboldlondon@gmail.com>
2021-09-14 11:36:19 +0100

    Secret message generator
    Secret message generator

---
+++ secret message.py
@@ -0,0 +1,15 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+newMessage = ' '
+
+message = input('Please enter a message: ')
+
+
+for character in message:
+    position = alphabet.find(character)
+    newPosition = (position + key) % 26
+    newCharacter = alphabet[newPosition]
+    newMessage += newCharacter
+    
+
+print(newMessage)
